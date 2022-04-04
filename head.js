537537cb5d417a09b24c8de2941e44b71d288072
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-04 14:53:04 -0400

    Added head function to lotide

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😬😬😬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (Array.isArray(array)) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined);
