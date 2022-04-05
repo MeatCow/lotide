aa856f851c4ded97a132fec849ec481dbc668507
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-05 02:01:56 -0400

    Implemented assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(actual, expected) {
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    if (actual.length === expected.length) {
+      return actual.every((value, index) => value === expected[index]);
+    }
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😬😬😬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
