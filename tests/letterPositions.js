0bcd9d14582b6006f97dedf5f73af861efca99c9
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 16:24:20 -0400

    Implemented tests for letterPositions

--- tests/letterPositions.js
+++ tests/letterPositions.js
@@ -2,20 +2,6 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { letterPositions } from "../lib/letterPositions.js";
 
-// const expected = {
-//   l: [0],
-//   i: [1, 11],
-//   g: [2],
-//   h: [3, 5, 15, 18],
-//   t: [4, 14],
-//   o: [6, 19],
-//   u: [7, 20],
-//   s: [8, 21],
-//   e: [9, 16, 22],
-//   n: [12]
-// };
-
-// const inputString = "lighthouse in the house";
 // const outputObject = letterPositions(inputString);
 
 // console.log(expected, outputObject);
@@ -23,9 +9,42 @@ import { letterPositions } from "../lib/letterPositions.js";
 // assertObjectArraysEqual(expected, outputObject);
 
 describe("letterPositions", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return an empty object for an empty input", () => {
+    assert.isEmpty(letterPositions(""));
+  });
+  it("should return an object of arrays for string input", () => {
+    const inputString = "lighthouse in the house";
+    const expected = {
+      l: [0],
+      i: [1, 11],
+      g: [2],
+      h: [3, 5, 15, 18],
+      t: [4, 14],
+      o: [6, 19],
+      u: [7, 20],
+      s: [8, 21],
+      e: [9, 16, 22],
+      n: [12]
+    };
+    assert.deepEqual(letterPositions(
+      inputString
+    ),expected
+    );
+  });
+  it("should not add punctuation into the output object, but placement should still be correctly calculated", () => {
+    const inputString = `lighthouse ++=in the ,./house"'.,,,/`;
+    const expected = {
+      l: [0],
+      i: [1, 14],
+      g: [2],
+      h: [3, 5, 18, 24],
+      t: [4, 17],
+      o: [6, 25],
+      u: [7, 26],
+      s: [8, 27],
+      e: [9, 19, 28],
+      n: [15]
+    };
+    assert.deepEqual(letterPositions(inputString), expected);
+  });
 });
