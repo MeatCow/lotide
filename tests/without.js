bd4af95f1e33e2e3634bedd6c2c1f33d198d1aa8
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 17:32:17 -0400

    Implemented additional test for without

--- tests/without.js
+++ tests/without.js
@@ -10,6 +10,7 @@ describe("without", () => {
   it("should return an array that removes the elements in the 2nd parameter", () => {
     assert.deepEqual(without([1,2,3],[2]), [1,3]);
     assert.deepEqual(without([1,2,3],[]), [1,2,3]);
+    assert.deepEqual(without([1,1,2,1,3],[1]), [2,3]);
   });
   it("should not modify the original array", () => {
     let original = ["Hello", "World"];
