1403ef9a58e34a11bc809e62a4a2874a325a3fae
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 15:26:59 -0400

    Implemented tests for eqObjects

--- tests/eqObjects.js
+++ tests/eqObjects.js
@@ -2,33 +2,51 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { eqObjects } from "../lib/eqObjects.js";
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba),true);
-
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
-
-// //Recursive test cases
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
-// assertEqual(eqObjects({ a: { z: 1, f: 3 }, b: 2 }, { a: { z: 1, f: 3 }, b: 2 }), true);
-// assertEqual(eqObjects({ a: { z: 1 }, cow: "moo" }, { a: { z: 1 }, cow: "moo" }), true);
-
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
-// assertEqual(eqObjects({ a: { y: 0, sheep: "bahhhh" }, b: 2 }, { a: {y: 0, cow: "mooo"}, b: 2 }), false);
-
 describe("eqObjects", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return true for 2 empty objects", () => {
+    assert.isTrue(eqObjects(
+      {},
+      {}
+    ));
+  });
+  it("should return true for 2 deeply equal objects", () => {
+    assert.isTrue(eqObjects(
+      { a: "1", b: "2" },
+      { a: "1", b: "2" }
+    ));
+    assert.isTrue(eqObjects(
+      { c: "1", d: ["2", 3] },
+      { d: ["2", 3], c: "1" }
+    ));
+    assert.isFalse(eqObjects(
+      { c: "1", d: ["2", 3, 4] },
+      { c: "1", d: ["2", 3] }
+    ));
+  });
+  it("should return true for 2 objects even if the keys are out of order", () => {
+    assert.isTrue(eqObjects(
+      { a: "1", b: "2" },
+      { b: "2", a: "1" }
+    ));
+  });
+  it("should return false for 2 different objects", () => {
+    assert.isFalse(eqObjects(
+      { a: "1", b: "1" },
+      { a: "1", b: "2" }
+    ));
+    assert.isFalse(eqObjects(
+      { a: "1", b: "2" },
+      { a: "1", b: "2", c: "3" }
+    ));
+  });
+  it("should return correctly for nested objects, as well as nested strings", () => {
+    assert.isTrue(eqObjects(
+      { a: { z: 1 }, b: 2 },
+      { a: { z: 1 }, b: 2 }
+    ));
+    assert.isFalse(eqObjects(
+      { a: { y: 0, sheep: "bahhhh" }, b: 2 },
+      { a: {y: 0, cow: "mooo"}, b: 2 }
+    ));
+  });
 });
