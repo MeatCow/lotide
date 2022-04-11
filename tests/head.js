86a53382f8fc3d09c09e43bc8c4ccb43a09c3ae2
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 01:14:46 -0400

    Implemented tests for head

--- tests/head.js
+++ tests/head.js
@@ -2,15 +2,30 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { head } from "../lib/head.js";
 
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([5]), 5);
 // assertEqual(head([]), undefined);
 
 describe("head", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return the first element in an array", () => {
+    assert.equal(head(
+      [5, 6, 7]
+    ),5
+    );
+  });
+  it("should return the first element in an array even if element is a string", () => {
+    assert.equal(head(
+      ["Hello", "Lighthouse", "Labs"]
+    ),"Hello"
+    );
+  });
+  it("should return the first element in an array, given an array with a single element", () => {
+    assert.equal(head(
+      [5]
+    ),5
+    );
+  });
+  it("should return undefined when given an empty array", () => {
+    assert.isUndefined(head(
+      []
+    ));
+  });
 });
