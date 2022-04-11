38b2bb052136f0858b844b7353e746eec61d3e50
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 14:30:20 -0400

    Implemented tests for countLetters

--- tests/countLetters.js
+++ tests/countLetters.js
@@ -23,9 +23,40 @@ import { countLetters } from "../lib/countLetters.js";
 // assertEqual(results, expected);
 
 describe("countLetters", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return an empty object for an empty string", () => {
+    assert.deepEqual(countLetters(
+      ""
+    ),{}
+    );
+  });
+  it(`should count a single letter "a" in a string containing only "a"`, () => {
+    assert.deepEqual(countLetters(
+      "a"
+    ), {a: 1}
+    );
+  });
+  it("should count all the letters in a string, including capital letters", () => {
+    const input = "This is my string, hear me ROAR!";
+    const expected = {
+      t: 2,
+      h: 2,
+      i: 3,
+      s: 3,
+      m: 2,
+      y: 1,
+      r: 4,
+      n: 1,
+      g: 1,
+      e: 2,
+      a: 2,
+      o: 1
+    };
+    assert.deepEqual(countLetters(input), expected);
+  });
+  it("should only count letters, and no other character", () => {
+    assert.deepEqual(countLetters(
+      ",./351=-+~"
+    ), {}
+    );
+  });
 });
