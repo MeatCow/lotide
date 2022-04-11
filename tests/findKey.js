29b3f7dee3a103a20d7ac3995ca2174d6fa5b587
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 15:38:22 -0400

    Implemented tests for findKey

--- tests/findKey.js
+++ tests/findKey.js
@@ -2,27 +2,36 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { findKey } from "../lib/findKey.js";
 
-// const hotels = {
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// };
-
-// const result = findKey(hotels, x => x.stars === 2);
-// const result2 = findKey(hotels, x => x.stars === 5);
-// const result3 = findKey(hotels, x => true);
-
-// assertEqual(result, "noma");
-// assertEqual(result2, undefined);
-// assertEqual(result3, "Blue Hill");
+const hotels = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
 
 describe("findKey", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return undefined for a predicate that always returns false", () => {
+    assert.isUndefined(findKey(
+      hotels,
+      x => false
+    ));
+  });
+  it("should return the first element in the object when the predicate evaluates to true", () => {
+    assert.strictEqual(findKey(
+      hotels,
+      x => true
+    ), "Blue Hill");
+    assert.strictEqual(findKey(
+      hotels,
+      x => x.stars === 2
+    ), "noma");
+  });
+  it("should return undefined when given an empty object", () => {
+    assert.isUndefined(findKey(
+      {},
+      x => x.stars === 2
+    ));
+  });
 });
