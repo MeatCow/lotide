5a26c77fd14d5226d0d0bf45be772053bc0c09e2
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 00:26:56 -0400

    Removed tests that targeted different module.

--- tests/assertArraysEqual.js
+++ tests/assertArraysEqual.js
@@ -4,7 +4,7 @@ import * as sinon from "sinon";
 import { assertArraysEqual } from "../lib/assertArraysEqual.js";
 
 describe("assertArraysEqual", () => {
-  it("should assert that 2 same arrays are equal", () => {
+  it("should console.log passed message when 2 arrays are the same", () => {
     const spy = sinon.spy(console, "log");
     const actual = [1, 2, 3];
     const expected = [1, 2, 3];
@@ -12,7 +12,7 @@ describe("assertArraysEqual", () => {
     assert(spy.calledWith(`👌👌👌 Assertion Passed: ${actual} === ${expected}`));
     spy.restore();
   });
-  it("should assert that 2 arrays with the same elements, in a different order, are not equal", () => {
+  it("should console.log failed message when 2 arrays are not the same", () => {
     const spy = sinon.spy(console, "log");
     const actual = [1, 2, 3];
     const expected = [3, 2, 1];
@@ -20,23 +20,7 @@ describe("assertArraysEqual", () => {
     assert(spy.calledWith(`😬😬😬 Assertion Failed: ${actual} !== ${expected}`));
     spy.restore();
   });
-  it("should assert correctly for arrays that contain strings", () => {
-    const spy = sinon.spy(console, "log");
-    const actual = ["1", "2", "3"];
-    const expected = ["1", "2", "3"];
-    assertArraysEqual(actual, expected);
-    assert(spy.calledWith(`👌👌👌 Assertion Passed: ${actual} === ${expected}`));
-    spy.restore();
-  });
-  it("should perform type comparison to ensure elements are the correct type", () => {
-    const spy = sinon.spy(console, "log");
-    const actual = ["1", "2", "3"];
-    const expected = ["1", "2", 3];
-    assertArraysEqual(actual, expected);
-    assert(spy.calledWith(`😬😬😬 Assertion Failed: ${actual} !== ${expected}`));
-    spy.restore();
-  });
-  it("should return true for empty arrays", () => {
+  it("should console.log a passed message with empty arrays", () => {
     const spy = sinon.spy(console, "log");
     const actual = [];
     const expected = [];
