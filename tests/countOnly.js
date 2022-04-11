612d0a3c112f1e5ed725ecc0f6e4ea3cd1000895
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 14:39:11 -0400

    Implemented tests for countOnly

--- tests/countOnly.js
+++ tests/countOnly.js
@@ -2,29 +2,44 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { countOnly } from "../lib/countOnly.js";
 
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
-
 describe("countOnly", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return an empty object when given empty inputs", () => {
+    assert.deepEqual(countOnly(
+      [],
+      {}
+    ),{}
+    );
+  });
+  it("should only return counts for the items in the filter list, anything else should be undefined", () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    const filters = {
+      "Jason": true,
+      "Karima": true,
+      "Fang": true,
+      "Agouhanna": false
+    };
+    const actual = countOnly(firstNames, filters);
+    assert.strictEqual(actual["Jason"], 1);
+    assert.strictEqual(actual["Fang"], 2);
+    assert.isUndefined(actual["Agouhanna"]);
+  });
+  it("should return an empty object for a fully false object", () => {
+    const names = ["Matt", "John"];
+    const filters = {"Matt": false, "John": false};
+    assert.deepEqual(countOnly(
+      names,
+      filters
+    ), {}
+    );
+  });
 });
