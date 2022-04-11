155d1839ed568b227a53c5839fd5dffa51e550dc
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 01:54:08 -0400

    Aggregated export through index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+import { assertArraysEqual } from './lib/assertArraysEqual';
+import { assertEqual } from './lib/assertEqual.js';
+import { assertObjectsEquals } from './lib/assertObjectsEquals.js';
+import { countLetters } from './lib/countLetters.js';
+import { countOnly } from './lib/countOnly.js';
+import { eqArrays } from './lib/eqArrays.js';
+import { eqObjects } from './lib/eqObjects.js';
+import { findKey } from './lib/findKey.js';
+import { findKeyByValue } from './lib/findKeyByValue.js';
+import { flatten } from './lib/flatten.js';
+import { head } from './lib/head.js';
+import { letterPositions } from './lib/letterPositions.js';
+import { map } from './lib/map.js';
+import { middle } from './lib/middle.js';
+import { tail } from './lib/tail.js';
+import { takeUntil } from './lib/takeUntil.js';
+import { without } from './lib/without.js';
+
+export {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEquals,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without};
