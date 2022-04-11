036fe26faaa3a7dbf2779b75f28f4fa0bf8b1f27
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-11 00:48:04 -0400

    Implemented tests for eqArrays.js

--- tests/eqArrays.js
+++ tests/eqArrays.js
@@ -2,21 +2,54 @@ import { describe } from "mocha";
 import { assert } from "chai";
 import { eqArrays } from "../lib/eqArrays.js";
 
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-// //Recursive tests
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-
 describe("eqArrays", () => {
-  it("");
-  it("");
-  it("");
-  it("");
-  it("");
+  it("should return true when 2, 1-depth arrays are equal", () => {
+    assert.isTrue(eqArrays(
+      [1, 2, 3],
+      [1, 2, 3]
+    ));
+  });
+  it("should return false when 2, 1-depth arrays are not equal", () => {
+    assert.isFalse(eqArrays(
+      [1, 2, 3],
+      [3, 2, 1]
+    ));
+  });
+  it("should return true when comparing arrays with string values", () => {
+    assert.isTrue(eqArrays(
+      ["1", "2", "3"],
+      ["1", "2", "3"]
+    ));
+  });
+  it("should return false when comparing a string to a number", () => {
+    assert.isFalse(eqArrays(
+      ["1", "2", "3"],
+      ["1", "2", 3]
+    ));
+  });
+  //Recursive tests below
+  it("should return true when comparing equivalent nested arrays", () => {
+    assert.isTrue(eqArrays(
+      [[2, 3], [4]],
+      [[2, 3], [4]]
+    ));
+  });
+  it("should return false when comparing different nested arrays", () => {
+    assert.isFalse(eqArrays(
+      [[2, 3], [4]],
+      [[2, 3], [4, 5]]
+    ));
+  });
+  it("should return false when comparing different nested arrays, even if the elements are the same, but not in an array", () => {
+    assert.isFalse(eqArrays(
+      [[2, 3], [4]],
+      [[2, 3], 4]
+    ));
+  });
+  it("should return true when comparing empty arrays", () => {
+    assert.isTrue(eqArrays(
+      [],
+      []
+    ));
+  });
 });
